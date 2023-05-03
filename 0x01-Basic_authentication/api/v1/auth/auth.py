11ddd9d22dd00e0d63adfd892e9c1358923d30f2
abelasfaw <abeloman89@gmail.com>
2023-05-03 01:06:32 -0700

    add function comments

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,17 +4,17 @@ from flask import request
 from typing import List, TypeVar
 
 
-
 class Auth:
     """ Class that handles api authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''validated required authorization'''
         return False
 
-
     def authorization_header(self, request=None) -> str:
+        '''handles authorization header'''
         return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
+        '''checks current user status'''
         return None
