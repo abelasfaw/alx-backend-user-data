7983404949945e49ba435ed5babed27d832512bf
abelasfaw <abeloman89@gmail.com>
2023-05-03 02:26:33 -0700

    added task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         '''handles authorization header'''
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''checks current user status'''
