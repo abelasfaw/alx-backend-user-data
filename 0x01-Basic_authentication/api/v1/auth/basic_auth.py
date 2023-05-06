e3ff240b6fdbeb1e9f9f8ae39de8a7c4f1e5d5be
abelasfaw <abeloman89@gmail.com>
2023-05-06 09:56:17 +0000

    added task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''Basic Authentication module'''
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        '''returns the decoded value of a Base64 string
+        base64_authorization_header'''
+
+        if (base64_authorization_header is None):
+            return None
+        if (not isinstance(base64_authorization_header, str)):
+            return None
+
+        try:
+            utf8_encoded = base64_authorization_header.encode('utf-8')
+            decoded = b64decode(utf8_encoded).decode('utf-8')
+        except BaseException:
+            return None
+        return decoded
