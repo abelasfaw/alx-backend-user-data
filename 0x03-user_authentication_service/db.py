2136de64a784aaa7634d23ada3b45c1084b97675
abelasfaw <abeloman89@gmail.com>
2023-05-11 16:59:58 +0000

    fix task 0

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,5 @@
-"""DB module
-"""
+#!/usr/bin/env python3
+"""DB module"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
